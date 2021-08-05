24acb7d01536f1610dab04381b09bd942743a3ba
Niaz Morshed <niazmorshed2007@gmail.com>
2021-08-05 15:55:45 +0600

    first commit

--- js/multimedia.js
+++ js/multimedia.js
@@ -3,22 +3,19 @@ var posters = document.querySelectorAll(".inner-home-videos > div");
 var video = document.querySelector("video");
 let counter = 1;
 
-// console.log(posters);
-// ex.
-// var poster = document.querySelectorAll(".poster");
-
 posters.forEach((e, i) => {
-  // counter = i;
-  // console.log(e);
-  e.addEventListener("click", showMultimedia);
-  e.addEventListener("click", () => {
-    counter = i + 1;
-    playVideo();
-  });
+  managingVideos(e, i);
 });
 
-function playVideo(i) {
-  // counter = i;
+function managingVideos(e, i) {
+  e.addEventListener("click", (event) => {
+    counter = event.target.alt || event.target.id;
+    playVideo();
+  });
+  e.addEventListener("click", showMultimedia);
+}
+
+function playVideo() {
   video.src = "video" + counter + ".mp4";
   video.play();
 }
