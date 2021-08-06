fe5fcc30f98239e9068f8755095678316981b1c0
Niaz Morshed <niazmorshed2007@gmail.com>
2021-08-06 15:44:45 +0600

    first commit

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-window.oncontextmenu = function (event) {
-  event.preventDefault();
-  event.stopPropagation();
-  return false;
-};
+// window.oncontextmenu = function (event) {
+//   event.preventDefault();
+//   event.stopPropagation();
+//   return false;
+// };
