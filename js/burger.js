90898eb46cea30ee1033ab642340e0a3087919f9
Niaz Morshed <niazmorshed2007@gmail.com>
2021-08-10 17:14:08 +0600

    first commit

--- js/burger.js
+++ js/burger.js
@@ -1,20 +1,20 @@
-var burger = document.querySelector(".burger");
-var modal = document.querySelector(".modal");
-var hiddenDiv = document.querySelector(".hidden-div");
+// var burger = document.querySelector(".burger");
+// var modal = document.querySelector(".modal");
+// var hiddenDiv = document.querySelector(".hidden-div");
 
-burger.addEventListener("click", showModal);
-document.body.addEventListener("click", (e) => {
-  hideModal(e);
-});
+// burger.addEventListener("click", showModal);
+// document.body.addEventListener("click", (e) => {
+//   hideModal(e);
+// });
 
-function showModal() {
-  modal.classList.add("modal-active");
-  hiddenDiv.classList.add("hidden-div-active");
-}
+// function showModal() {
+//   modal.classList.add("modal-active");
+//   hiddenDiv.classList.add("hidden-div-active");
+// }
 
-function hideModal(e) {
-  if (e.target.classList.contains("modal")) {
-    modal.classList.remove("modal-active");
-    hiddenDiv.classList.remove("hidden-div-active");
-  }
-}
+// function hideModal(e) {
+//   if (e.target.classList.contains("modal")) {
+//     modal.classList.remove("modal-active");
+//     hiddenDiv.classList.remove("hidden-div-active");
+//   }
+// }
--- js/media.js
+++ js/media.js
@@ -1,25 +1,35 @@
-// var progressWrapper = document.querySelector(".progress-wrapper");
+var progressWrapper = document.querySelector(".progress-wrapper");
 
-// video.addEventListener("click", videoControlOnClick);
+function myFunction() {
+  if (x.matches) {
+    // video.addEventListener("play", progressWrapperActiveAuto);
+    // video.addEventListener("pause", progressWrapperActiveAuto);
+    // video.addEventListener("click", progressWrapperActiveOnClick);
+    // If media query matches
+  } else {
+    document.body.style.display = "block";
+  }
+}
 
-// function myFunction() {
-//   if (x.matches) {
-//     // If media query matches
-//   } else {
-//     document.body.style.display = "block";
-//   }
-// }
-
-// var x = window.matchMedia("(min-aspect-ratio: 16/9)");
-// myFunction(x);
-// x.addListener(myFunction);
+var x = window.matchMedia("(min-aspect-ratio: 16/9)");
+myFunction(x);
+x.addListener(myFunction);
 
 // var timeOut;
 
-// function videoControlOnClick() {
-//   progressWrapper.classList.add("progress-active");
+// function progressWrapperActiveAuto() {
+//   this.paused
+//     ? progressWrapper.classList.add("progress-wrapper-active")
+//     : progressWrapper.classList.remove("progress-wrapper-active");
+//   this.paused ? clearTimeout(timeOut) : console.log("playing");
+// }
+
+// function progressWrapperActiveOnClick() {
+//   // const videoOverlay = document.querySelector(".video-overlay");
+//   progressWrapper.classList.add("progress-wrapper-active");
+//   console.log("overlay");
 
 //   timeOut = setTimeout(() => {
-//     progressWrapper.classList.remove("progress-active");
+//     progressWrapper.classList.remove("progress-wrapper-active");
 //   }, 1300);
 // }
