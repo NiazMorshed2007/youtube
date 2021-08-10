5f61849b2d292c75b6792bf677c655f05b2a77f1
Niaz Morshed <niazmorshed2007@gmail.com>
2021-08-10 17:36:23 +0600

    first commit

--- js/media.js
+++ js/media.js
@@ -1,35 +1,65 @@
 var progressWrapper = document.querySelector(".progress-wrapper");
 
-function myFunction() {
-  if (x.matches) {
-    // video.addEventListener("play", progressWrapperActiveAuto);
-    // video.addEventListener("pause", progressWrapperActiveAuto);
-    // video.addEventListener("click", progressWrapperActiveOnClick);
-    // If media query matches
+// function myFunction() {
+//   if (x.matches) {
+//     video.addEventListener("play", progressWrapperOnVideo);
+//     video.addEventListener("pause", progressWrapperOnVideo);
+//     video.addEventListener("click", progressWrapperAppear);
+//     // If media query matches
+//   } else {
+//     document.body.style.display = "block";
+//   }
+// }
+
+// var x = window.matchMedia("(min-aspect-ratio: 16/9)");
+// myFunction(x);
+// x.addListener(myFunction);
+
+// // var timeOut;
+
+// // function progressWrapperActiveAuto() {
+// //   this.paused
+// //     ? progressWrapper.classList.add("progress-wrapper-active")
+// //     : progressWrapper.classList.remove("progress-wrapper-active");
+// //   this.paused ? clearTimeout(timeOut) : console.log("playing");
+// // }
+
+// // function progressWrapperActiveOnClick() {
+// //   // const videoOverlay = document.querySelector(".video-overlay");
+// //   progressWrapper.classList.add("progress-wrapper-active");
+// //   console.log("overlay");
+
+// //   timeOut = setTimeout(() => {
+// //     progressWrapper.classList.remove("progress-wrapper-active");
+// //   }, 1300);
+// // }
+const mediaQuery = window.matchMedia("(min-aspect-ratio: 16/9)");
+// Check if the media query is true
+function media() {
+  if (mediaQuery.matches) {
+    var progressTimeout;
+
+    function progressWrapperAppear() {
+      progressWrapper.classList.add("progress-wrapper-active");
+
+      progressTimeout = setTimeout(() => {
+        progressWrapper.classList.remove("progress-wrapper-active");
+      }, 1300);
+    }
+
+    function progressWrapperOnVideo() {
+      this.paused
+        ? progressWrapper.classList.add("progress-wrapper-active")
+        : progressWrapper.classList.remove("progress-wrapper-active");
+      this.paused ? clearTimeout(progressTimeout) : console.log("playing>..");
+    }
+    video.addEventListener("play", progressWrapperOnVideo);
+    video.addEventListener("pause", progressWrapperOnVideo);
+    video.addEventListener("click", progressWrapperAppear);
+    console.log("matched");
   } else {
-    document.body.style.display = "block";
+    console.log("not-matched");
   }
 }
 
-var x = window.matchMedia("(min-aspect-ratio: 16/9)");
-myFunction(x);
-x.addListener(myFunction);
-
-// var timeOut;
-
-// function progressWrapperActiveAuto() {
-//   this.paused
-//     ? progressWrapper.classList.add("progress-wrapper-active")
-//     : progressWrapper.classList.remove("progress-wrapper-active");
-//   this.paused ? clearTimeout(timeOut) : console.log("playing");
-// }
-
-// function progressWrapperActiveOnClick() {
-//   // const videoOverlay = document.querySelector(".video-overlay");
-//   progressWrapper.classList.add("progress-wrapper-active");
-//   console.log("overlay");
-
-//   timeOut = setTimeout(() => {
-//     progressWrapper.classList.remove("progress-wrapper-active");
-//   }, 1300);
-// }
+setInterval(media, 1000);
